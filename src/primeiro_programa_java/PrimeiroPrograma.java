8b104f94600c5285b5127058e73518cd2b63c42c
Fernando Rubini <fernandorubini@hotmail.com>
2023-03-15 16:02:10 -0300

    first commit

---
+++ src/primeiro_programa_java/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package primeiro_programa_java;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		System.out.println("Hello World! " + (a+b));
+}
+	
+}
